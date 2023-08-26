92da8b69d01112c6a1c1ca7a83f5c4a6ac552ac4
vengadeshrasu <143246036+vengadeshrasu@users.noreply.github.com>
2023-08-26 11:04:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-
+function display(val){
+    document.getElementById('result').value += val;
+}
+function solve(){
+    let x = document.getElementById('result').value
+    let y = eval(x);
+    document.getElementById('result').value = y;
+}
+function clearscreen(){
+    document.getElementById('result').value = '';
+}
